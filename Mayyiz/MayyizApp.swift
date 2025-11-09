42b63bc4defa0a26eafe6033dcce8589a8b504f5
Bola Samy <bolasamy0123@gmail.com>
2025-11-09 21:17:22 +0200

    Initial Commit

---
+++ Mayyiz/MayyizApp.swift
@@ -0,0 +1,17 @@
+//
+//  MayyizApp.swift
+//  Mayyiz
+//
+//  Created by Bola Samy on 09/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct MayyizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MayyizTests/MayyizTests.swift
@@ -0,0 +1,16 @@
+//
+//  MayyizTests.swift
+//  MayyizTests
+//
+//  Created by Bola Samy on 09/11/2025.
+//
+
+import Testing
+
+struct MayyizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
